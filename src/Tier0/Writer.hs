4825e024bcd53cb4672fa7220f2f1edeeaf751bf
solnyshko3622 <125785938+solnyshko3622@users.noreply.github.com>
2023-03-06 22:34:10 +0300

    Update Writer.hs

--- src/Tier0/Writer.hs
+++ src/Tier0/Writer.hs
@@ -5,4 +5,18 @@ import Control.Monad.Writer
 data Tree a = Leaf a | Branch (Tree a) a (Tree a) deriving Eq
 
 sumAndTraceInOrder :: Num a => Tree a -> Writer [a] a
-sumAndTraceInOrder = undefined
+sumAndTraceInOrder (Leaf x) = writer (x, [x])
+sumAndTraceInOrder (Branch l v r) = do
+  tell $ return v
+  x <- sumAndTraceInOrder l
+  y <- sumAndTraceInOrder r
+  return $ x + v + y
+
+
+
+sum' :: Int -> Int -> Writer String Int
+sum' x y = do
+  tell $ "first operand: " ++ show x ++ "\n"
+  tell $ "second operand: " ++ show y ++ "\n"
+  return $ x + y
+
