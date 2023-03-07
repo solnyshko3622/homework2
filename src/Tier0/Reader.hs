78f373aa90b9ed58960cb8e2620d58953b9fc9e9
solnyshko3622 <125785938+solnyshko3622@users.noreply.github.com>
2023-03-07 14:15:02 +0300

    Update Reader.hs

--- src/Tier0/Reader.hs
+++ src/Tier0/Reader.hs
@@ -9,18 +9,20 @@ type EnvironmentM = Reader Environment
 
 formatUserName :: EnvironmentM String
 formatUserName = do 
-  nameMesage <- name
-  isSuperMesage <- super 
-  hostMesage <- hostm 
-  currentDirMesage <- currentDir
-  return $ [nameMesage, isSuperMesage, hostMesage, currentDirMesage]
-
-formatHost :: EnvironmentM String
-formatHost = do 
   x <- ask
-  if x == "root" 
-    then return $ "root"
-    else return $ username
+  if (isSuperUser x)
+  then return "root"
+  else return (username x)
+  
+  
+  
+  
+  
+formatHost :: EnvironmentM String
+formatHost = do
+  x <- ask
+  return $ (host x)
+
 
 formatCurrentDir :: EnvironmentM String
 formatCurrentDir = do
@@ -29,8 +31,10 @@ formatCurrentDir = do
 
 formatPrompt :: EnvironmentM String
 formatPrompt = do
-  x <- ask
-  return $ "{" ++ username ++ "}@{" ++ host ++"}:{" ++ currentDir ++ "}"
+  name <- formatUserName
+  host <- formatHost
+  cwd <- formatCurrentDir
+  return $ "{" ++ name ++ "}@{" ++ host ++"}:{" ++ cwd ++ "}"
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   ``````````````````````````````````````````````````````````````````````````````````````module Tier0.Reader (Environment (..), EnvironmentM, formatUserName, formatHost, formatCurrentDir, formatPrompt) where
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   import Control.Monad.Reader
